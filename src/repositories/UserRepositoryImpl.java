42314236b8385e95100cf20f32b7fb1d9f6ed34a
damianleguina <leguinadamian@gmail.com>
2023-10-17 14:54:54 -0300

    Added BaseRepository

--- src/repositories/UserRepositoryImpl.java
+++ src/repositories/UserRepositoryImpl.java
@@ -3,38 +3,24 @@ package repositories;
 import entities.User;
 import repositories.interfaces.UserRepository;
 
-public class UserRepositoryImpl implements UserRepository {
-    private final User[] data;
+public class UserRepositoryImpl extends BaseRepository<User> implements UserRepository {
 
     public UserRepositoryImpl() {
-        data = new User[4];
+        super(new User[4]);
         data[0] = new User(1, "Blinky", "qwerty", true);
         data[1] = new User(1, "Pinky", "dvorak", false);
         data[2] = new User(1, "Inky", "colemak", false);
         data[3] = new User(1, "Clyde", "qwerty", false);
     }
 
-    @Override
-    public User getById(long id) {
-        for (User user : data) {
-            if (user.getId() == id) {
-                return user;
-            }
-        }
-        return null;
-    }
-
-    @Override
     public User add(User entity) {
         throw new UnsupportedOperationException("Unimplemented method 'add'");
     }
 
-    @Override
     public User update(User entity) {
         throw new UnsupportedOperationException("Unimplemented method 'update'");
     }
 
-    @Override
     public boolean delete(long id) {
         for (User user : data) {
             if (user.getId() == id) {
@@ -45,7 +31,6 @@ public class UserRepositoryImpl implements UserRepository {
         return false;
     }
 
-    @Override
     public User getByCredentials(String name, String password) {
         for (User user : data) {
             if (user.getName() == name && user.getPassword() == password) {
